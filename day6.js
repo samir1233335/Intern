3b337e989d04a026e396f5b361cf388cef588a4e
Samir rait <np05cp4a240363@iic.edu.np>
2026-06-29 17:28:38 +0545

    fix: correct variable declaration and update logical operator in day 6 commit

---
+++ day6.js
@@ -0,0 +1,99 @@
+
+
+// variable understand
+// var age =22;
+// var age =23;
+// let age= 23;
+// let  age =24;
+// console.log(age);
+// const pie=3.14;
+
+
+
+// data types
+let number =23.567; // number 
+const name ="Rahultharu064"; //string
+let isloggedin =true; // boolean
+let city;
+let user= null;
+ console.log(typeof name);
+ console.log(typeof city);
+
+ //operations
+
+ //arithmetic operations
+ let x=4
+ let y=6;
+ let sum=x+y
+ console.log(sum) // addition 
+
+ let r=14;
+ let b=10;
+ console.log(r-b); //subtraction
+ console.log(r*b); // multiplication
+ console.log(r/b); // division
+ console.log(r%b);
+
+ // comparison operators  is albout true or false 
+ // == equal to
+ // != not equal to
+ // > greater than
+ // < less than
+// >= greater  than equal to 
+// <= less than equal to 
+// === strict equal to
+
+let age = 22;
+ console.log(age==22); // checking equal to 
+ console.log(age!=22); // checking not equal to 
+ console.log(age>=45);  // checking greater than equal to 
+ console.log(age<=10); // checkimg  less than equal to
+
+
+ // logical operators
+//%% ||
+// ||  
+ let ag=22;
+ let citizen=false;
+ console.log(ag>=18 && citizen==true); //example of the logical operators
+
+ // assignment operators is all about increment and decrement
+ let a=5;
+ a-=20;
+ console.log(a)
+
+
+ /// conditions
+  let age44=10;
+
+  // voting elegible xu ki xuena
+
+  if(age44>=18){
+    console.log("yes you are eleigible for the voting");
+  }
+  else{
+    console.log("no you are not elegible for the voting");
+  }
+
+
+  //  else if
+  let mern=60;
+  if(mern>=90){
+    console.log("you are getting A+")
+  }
+  else if (mern >=80){
+    console.log("you are getting A")
+  }
+  else{
+    console.log("you are average student")
+  }
+
+
+
+
+ 
+
+
+
+
+
