3ec1161983e16747c893aa44f28c83710a5025df
Samir rait <np05cp4a240363@iic.edu.np>
2026-07-14 09:15:27 +0545

    learning hooks,state,usestate and useeffect Commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,47 @@
-import React from 'react'
-import { useState } from 'react'
+// import React from 'react'
+// import { useState } from 'react'
 
+
+// const App = () => {
+//   const [count,setCount]=useState(1)
+//   function increase(){
+//     setCount(count+1);
+//   }
+//   return (
+//     <>
+//     <h1>{count}</h1>
+//     <button onClick={increase} className='bg-amber-800 text-blue-800'>increment</button>
+    
+      
+//     </>
+//   )
+// }
+/// example of the useEffect
+// export default App
+// setIntervale is used to repeatedly execute a function or a code snippet at fixed time intervals
+
+import React, { useEffect, useState } from 'react'
 
 const App = () => {
-  const [count,setCount]=useState(0)
-  function increase(){
-    setCount(count+1);
-  }
+  const[seconds,setSeconds]=useState(0);
+  const[running,setRunning]=useState(false)
+  useEffect(()=>{
+    let timer;
+    if(running){
+      timer=setInterval(()=>{
+        setSeconds((prev)=> prev+1);
+
+      },1000)
+    }
+    return()=> clearInterval(timer)
+
+
+  },[running]) 
   return (
     <>
-    <h1>{count}</h1>
-    <button onClick={increase} className='bg-amber-800 text-blue-800'>increment</button>
-    
+    <h1>{seconds}</h1>
+    <button  onClick={()=> setRunning(true)}>start</button>
+    <button  onClick={()=> setRunning(false)}>stop</button>
       
     </>
   )
